044efce32e74596302d6306d9ec30ab47132f133
hungnk54 <hungnvk54@gmail.com>
2015-01-28 01:26:10 +0700

    create timer driver for MSP430 platform

---
+++ header/timer.h
@@ -0,0 +1,6 @@
+#ifndef __TIMER_H_
+#define __TIMER_H_
+
+FUNC(Std_ReturnType, FUNC)Timer_Init(uint8 idx);
+
+#endif
---
+++ source/timer.c
@@ -0,0 +1,6 @@
+#include "../header/timer.h"
+
+FUNC(Std_ReturnType, FUNC)Timer_Init(uint8 idx)
+{
+  
+}
